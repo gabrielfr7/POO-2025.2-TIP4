04d1eb7a9d21bd6d8df7b427bc7a0ebe2e99a65d
Gabriel Freire <freire.anjos08@aluno.ifce.edu.br>
2025-08-25 22:00:25 -0300

    Add files via upload

---
+++ Lista-01/Exercicios-Propostos/Q05/src/br/edu/principal/Principal.java
@@ -0,0 +1,12 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		double preco = 100.0;
+		
+		System.out.println("O novo preço com desconto é: " + (preco - (preco * 0.10)));
+
+	}
+
+}
